34e6e32ce066e9f65f66f2ed5a3fd1b7ac821276
Ankit Shah <ankit.tronix@gmail.com>
2018-01-24 17:41:57 +0000

    Updating flethc installtion

--- install_fletch.sh
+++ install_fletch.sh
@@ -3,3 +3,5 @@ mkdir fletch
 cd fletch/
 git clone https://github.com/Kitware/fletch.git src
 mkdir -p build/rel
+cd build/rel
+cmake -DCMAKE_BUILD_TYPE=Release -Dfletch_ENABLE_ALL_PACKAGES=ON -Dfletch_ENABLE_PYTHON=ON ../../src
