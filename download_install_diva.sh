dfaa48f695e74f27d1555845be2437e5927a0fb0
Ankit Shah <ankit.tronix@gmail.com>
2018-01-24 01:46:57 +0000

    Diva installation script

---
+++ download_install_diva.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+mkdir diva
+cd diva
+git clone https://github.com/Kitware/diva src
+mkdir -p build/release
+cd build/release
+cmake ../../src/ -DCMAKE_BUILD_TYPE=Release
+make -j16
+make install
