dddfea9d5343f4357ddcb07507a7bf49e336daf0
Ankit Shah <ankshah.mail@gmail.com>
2018-01-25 19:24:50 -0500

    adding python dependency for installation

--- fletch_dependencies.sh
+++ fletch_dependencies.sh
@@ -4,4 +4,5 @@ yes | apt-get install libexpat1-dev
 yes | apt-get install libgtk2.0-dev
 yes | apt-get install liblapack-dev
 yes | apt-get install libxt-dev
+yes | apt-get install python2.7-dev
 yes | apt install cmake
