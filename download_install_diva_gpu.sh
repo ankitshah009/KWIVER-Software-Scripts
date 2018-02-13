76d50f4b3b29e227a412b1f5865cb82ee2488c9d
Ankit Shah <ankit.tronix@gmail.com>
2018-02-13 17:27:59 +0000

    Committing file with the DC Install make path

--- download_install_diva_gpu.sh
+++ download_install_diva_gpu.sh
@@ -4,5 +4,5 @@ cd diva_gpu
 git clone https://github.com/Kitware/diva src
 mkdir -p build/release
 cd build/release
-cmake ../../src/ -DCMAKE_BUILD_TYPE=Release -DDIVA_BUILD_WITH_CUDA=ON
+cmake ../../src/ -DCMAKE_BUILD_TYPE=Release -DDIVA_BUILD_WITH_CUDA=ON -DCMAKE_INSTALL_PREFIX=/data/sdd/aps1/diva_gpu_install
 make
